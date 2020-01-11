9f6f479bc3b74658c9b5a6b0121b1b1f987a6fc1
Pierre-Adrien Ponthieu <pierre-adrien.ponthieu@epita.fr>
2020-01-11 16:36:15 +0100

    render.cu with shared memory

--- project/src/render.cu
+++ project/src/render.cu
@@ -180,14 +180,152 @@ __global__ void kernel_erosion_sep_2(unsigned char* dst_device, unsigned char *t
     dst_device[y*width+x] = 0;
 }
 
+__global__ void kernel_erosion_sep_1_shared(unsigned char* tmp_device, unsigned char *src_device,
+    int structuring_radius, int width, int height, int tile_width, int tile_height)
+{
+  //pixel in the image
+  int x = blockIdx.x * tile_width + threadIdx.x;
+  int y = blockIdx.y * tile_height + threadIdx.y - structuring_radius;
+
+  if (x >= width || y < 0 || x < 0 || y >= height) {
+    return;
+  }
+  extern __shared__ unsigned char shared[];
+
+  //fill shared memory with image data
+  shared[threadIdx.y * blockDim.x + threadIdx.x] = src_device[y * width + x];
+  //wait for all threads
+  __syncthreads();
+  if (y < (blockIdx.y * tile_height) || blockIdx.y >= ((blockIdx.y + 1) * tile_height)) {
+    return;
+  }
+
+  //starting point on the y axis for the structuring element
+  int offset = threadIdx.y - structuring_radius;
+
+  //starting point in shared memory
+  unsigned char *start = &shared[offset * blockDim.x + threadIdx.x];
+
+  bool stop = false;
+
+  //iterate on the cols for a 1D convolution along the y axis
+  for (int i = 0; i <= 2 * structuring_radius && !stop; i++)
+  {
+    if (start[i * blockDim.x] == 255)
+    {
+      tmp_device[y * width + x] = 255;
+      stop = true;
+    }
+  }
+
+  if (!stop)
+    tmp_device[y * width + x] = 0;
+}
+
+__global__ void kernel_erosion_sep_2_shared(unsigned char* tmp_device, unsigned char *src_device,
+    int structuring_radius, int width, int height, int tile_width, int tile_height)
+{
+
+  int x = blockIdx.x * tile_width + threadIdx.x - structuring_radius;
+  int y = blockIdx.y * tile_height + threadIdx.y;
+
+  if (x >= width || y < 0 || x < 0 || y >= height) {
+    return;
+  }
+  extern __shared__ unsigned char shared[];
+  shared[threadIdx.y * blockDim.x + threadIdx.x] = src_device[y * width + x];
+  __syncthreads();
+  if (x < (blockIdx.x * tile_width) || blockIdx.x >= ((blockIdx.x + 1) * tile_width)) {
+    return;
+  }
+
+  unsigned char *start = &shared[threadIdx.y * blockDim.x + threadIdx.x - structuring_radius];
+
+  bool stop = false;
+  for (int i = 0; i <= 2 * structuring_radius && !stop; i++)
+  {
+    if (start[i] == 255)
+    {
+      tmp_device[y * width + x] = 255;
+      stop = true;
+    }
+  }
+
+  if (!stop)
+    tmp_device[y * width + x] = 0;
+}
+
+
+__global__ void kernel_dilation_sep_1_shared(unsigned char* tmp_device, unsigned char *src_device,
+    int structuring_radius, int width, int height, int tile_width, int tile_height)
+{
+  int x = blockIdx.x * tile_width + threadIdx.x;
+  int y = blockIdx.y * tile_height + threadIdx.y - structuring_radius;
+
+  if (x >= width || y < 0 || x < 0 || y >= height) {
+    return;
+  }
+  extern __shared__ unsigned char shared[];
+  shared[threadIdx.y * blockDim.x + threadIdx.x] = src_device[y * width + x];
+  __syncthreads();
+  if (y < (blockIdx.y * tile_height) || blockIdx.y >= ((blockIdx.y + 1) * tile_height)) {
+    return;
+  }
+  int offset = threadIdx.y - structuring_radius;
+  unsigned char *start = &shared[offset * blockDim.x + threadIdx.x];
+
+  bool stop = false;
+  for (int i = 0; i <= 2 * structuring_radius && !stop; i++)
+  {
+    if (start[i * blockDim.x] == 0)
+    {
+      tmp_device[y * width + x] = 0;
+      stop = true;
+    }
+  }
+
+  if (!stop)
+    tmp_device[y * width + x] = 255;
+}
+
+__global__ void kernel_dilation_sep_2_shared(unsigned char* tmp_device, unsigned char *src_device,
+    int structuring_radius, int width, int height, int tile_width, int tile_height)
+{
+
+  int x = blockIdx.x * tile_width + threadIdx.x - structuring_radius;
+  int y = blockIdx.y * tile_height + threadIdx.y;
+
+  if (x >= width || y < 0 || x < 0 || y >= height) {
+    return;
+  }
+  extern __shared__ unsigned char shared[];
+  shared[threadIdx.y * blockDim.x + threadIdx.x] = src_device[y * width + x];
+  __syncthreads();
+  if (x < (blockIdx.x * tile_width) || blockIdx.x >= ((blockIdx.x + 1) * tile_width)) {
+    return;
+  }
+
+  unsigned char *start = &shared[threadIdx.y * blockDim.x + threadIdx.x - structuring_radius];
+
+  bool stop = false;
+  for (int i = 0; i <= 2 * structuring_radius && !stop; i++)
+  {
+    if (start[i] == 0)
+    {
+      tmp_device[y * width + x] = 0;
+      stop = true;
+    }
+  }
+
+  if (!stop)
+    tmp_device[y * width + x] = 255;
+}
+
 void run_kernel(std::string type, unsigned char* dst_device, unsigned char* src_device,
     int width, int height, int structuring_radius, std::ptrdiff_t stride)
 {
-  // Allocate device memory
   int bsize = 2;
   dim3 block(bsize, bsize);
-
-  // returns the best pitch (padding) which is the new row
 
   int size_x = std::ceil((float)width / bsize);
   int size_y = std::ceil((float)height / bsize);
@@ -211,12 +349,50 @@ void run_kernel(std::string type, unsigned char* dst_device, unsigned char* src_
       kernel_dilation_sep_2<<<grid, block>>>(dst_device, tmp_device, structuring_radius, width, height);
 
     }
-    else
+    else if (!type.compare("erosion_sep"))
     {
       kernel_erosion_sep_1<<<grid, block>>>(tmp_device, src_device, structuring_radius, width, height);
       cudaError_t cudaerr = cudaDeviceSynchronize();
       kernel_erosion_sep_2<<<grid, block>>>(dst_device, tmp_device, structuring_radius, width, height);
     }
+    else if (!type.compare("erosion_sep_shared"))
+    {
+      int tile_w = 1;
+      int tile_h = height;
+      dim3 block_shared_y(tile_w, tile_h);
+      size_x = std::ceil((float) width / tile_w);
+      size_y = std::ceil((float) width / tile_h);
+      dim3 grid_shared_y(size_x, size_y);
+      kernel_erosion_sep_1_shared<<<grid_shared_y, block_shared_y, tile_w*tile_h*sizeof(unsigned char)>>>(tmp_device, src_device, structuring_radius, width, height, tile_w, tile_h);
+      cudaError_t cudaerr = cudaDeviceSynchronize();
+      tile_w = width;
+      tile_h = 1;
+      dim3 block_shared_x(tile_w, tile_h);
+      size_x = std::ceil((float) width / tile_w);
+      size_y = std::ceil((float) width / tile_h);
+      dim3 grid_shared_x(size_x, size_y);
+      kernel_erosion_sep_2_shared<<<grid_shared_x, block_shared_x, tile_w*tile_h*sizeof(unsigned char)>>>(dst_device, tmp_device, structuring_radius, width, height, tile_w, tile_h);
+
+    }
+    else if (!type.compare("dilation_sep_shared"))
+    {
+      int tile_w = 1;
+      int tile_h = height;
+      dim3 block_shared_y(tile_w, tile_h);
+      size_x = std::ceil((float) width / tile_w);
+      size_y = std::ceil((float) width / tile_h);
+      dim3 grid_shared_y(size_x, size_y);
+      kernel_dilation_sep_1_shared<<<grid_shared_y, block_shared_y, tile_w*tile_h*sizeof(unsigned char)>>>(tmp_device, src_device, structuring_radius, width, height, tile_w, tile_h);
+      cudaError_t cudaerr = cudaDeviceSynchronize();
+      tile_w = width;
+      tile_h = 1;
+      dim3 block_shared_x(tile_w, tile_h);
+      size_x = std::ceil((float) width / tile_w);
+      size_y = std::ceil((float) width / tile_h);
+      dim3 grid_shared_x(size_x, size_y);
+      kernel_dilation_sep_2_shared<<<grid_shared_x, block_shared_x, tile_w*tile_h*sizeof(unsigned char)>>>(dst_device, tmp_device, structuring_radius, width, height, tile_w, tile_h);
+    }
+
   }
 
   cudaError_t cudaerr = cudaDeviceSynchronize();
